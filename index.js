d1f2bc108607330e5bafb77de296ff6a71eabbe7
MasumRezaMunna <masumrezamunna2005@gmail.com>
2026-01-19 19:15:44 +0600

    updated user to database

--- index.js
+++ index.js
@@ -49,6 +49,22 @@ async function run() {
       res.send(result);
     });
 
+    app.patch('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const updatedUser = req.body;
+      console.log('to update', id, updatedUser)
+      const query = {_id: new ObjectId(id)}
+      const update = {
+        $set: {
+          name: updatedUser.name,
+          email: updatedUser.email
+        }
+      }
+      const options = {}
+      const result = await usersCollection.updateOne(query, update, options);
+      res.send(result)
+    })
+
     app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
